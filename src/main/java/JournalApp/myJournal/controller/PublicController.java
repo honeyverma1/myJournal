31b22ad2907835445e00b52cef45935c0bebd9c7
honeyverma1 <honeyverma8240@gmail.com>
2025-08-15 20:15:20 +0530

    implemented the jwt login functionality. made jwt filter to run before default auth and removed basic auth from SpringSecurity

--- src/main/java/JournalApp/myJournal/controller/PublicController.java
+++ src/main/java/JournalApp/myJournal/controller/PublicController.java
@@ -1,11 +1,16 @@
 package JournalApp.myJournal.controller;
 
 import JournalApp.myJournal.entity.User;
+import JournalApp.myJournal.service.UserDetailsServiceImpl;
 import JournalApp.myJournal.service.UserService;
+import JournalApp.myJournal.utils.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,15 +18,40 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/public")
+@Slf4j
 public class PublicController {
 
     @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
     private UserService userService;
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
 
-    @PostMapping("/create-user")
-    public ResponseEntity<?> createUser(@RequestBody User user){
+    @Autowired
+    private JwtUtil jwtUtil;
+
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody User user){
         userService.saveNewUser(user);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user){
+        try{
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())
+            );
+            UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUsername());
+            String jwt = jwtUtil.generateToken(userDetails.getUsername());
+            return new ResponseEntity<>(jwt,HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Exception Occured while createAuthenticationToken", e);
+            return new ResponseEntity<>("Incorrect username or password", HttpStatus.BAD_REQUEST);
+        }
+    }
+
 
 }
---
+++ src/main/java/JournalApp/myJournal/utils/JwtUtil.java
@@ -0,0 +1,70 @@
+package JournalApp.myJournal.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtUtil {
+
+    private String SECRET_KEY = "c228c4a23f17bf1233113ccfc7e75516908d3a5c06ab3fff3f69191a44c8d15e";
+
+    public String extractUsername(String token) {
+        Claims claims = extractAllClaims(token);
+        return claims.getSubject();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(username, claims);
+    }
+
+    private String createToken(String subject, Map<String, Object> claims) {
+        return Jwts.builder()
+                .claims(claims)
+                .subject(subject)
+                .header().empty().add("typ", "JWT")
+                .and()
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(getSignKey())
+                .compact();
+
+    }
+
+    public Date extractExpiration(String token) {
+        return extractAllClaims(token).getExpiration();
+    }
+
+    private Key getSignKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public boolean validateToken(String token) {
+        return !isTokenExpired(token);
+    }
+}
